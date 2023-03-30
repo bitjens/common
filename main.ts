86e710b0981e54213c5dfd4ce3c7db43f0d2b7e9
bitjens <127223802+bitjens@users.noreply.github.com>
2023-03-30 09:13:58 +0200

    Opdater main.ts

--- main.ts
+++ main.ts
@@ -6,14 +6,14 @@
 namespace bitjens {
 
     export enum BeepCode {
-        //% enumval=0 block=Slut
-        End = 0,
-        //% enumval=1 block=Start
-        Begin = 1,
-        //% enumval=2 block=Afvis
-        Reject = 2,
-        //% enumval=3 block=Accepter
-        Accept = 3,
+        //% enumval=0 block=Start
+        Begin = 0,
+        //% enumval=1 block=Slut
+        End = 1,
+        //% enumval=2 block=Accepter
+        Accept = 2,
+        //% enumval=3 block=Afvis
+        Reject = 3,
     }
 
     //% block="BitJens: Afspil bip-kode $code"
@@ -21,6 +21,13 @@ namespace bitjens {
     //% code.defl=bitjens.BeepCode.Begin
     export function beepcode(code: BeepCode) {
         switch (code) {
+            case BeepCode.Begin:
+                music.playTone(1000, 90)
+                basic.pause(50)
+                music.playTone(1200, 90)
+                basic.pause(50)
+                music.playTone(1500, 400)
+                break;
             case BeepCode.End:
                 music.playTone(1500, 90)
                 basic.pause(50)
@@ -28,12 +35,12 @@ namespace bitjens {
                 basic.pause(50)
                 music.playTone(1000, 400)
                 break;
-            case BeepCode.Begin:
-                music.playTone(1000, 90)
+            case BeepCode.Accept:
+                music.playTone(1000, 200)
                 basic.pause(50)
-                music.playTone(1000, 90)
+                music.playTone(1500, 90)
                 basic.pause(50)
-                music.playTone(1500, 400)
+                music.playTone(1500, 90)
                 break;
             case BeepCode.Reject:
                 music.playTone(1500, 200)
@@ -41,13 +48,6 @@ namespace bitjens {
                 music.playTone(1000, 90)
                 basic.pause(50)
                 music.playTone(1000, 90)
-                break;
-            case BeepCode.Accept:
-                music.playTone(1000, 200)
-                basic.pause(50)
-                music.playTone(1500, 90)
-                basic.pause(50)
-                music.playTone(1500, 90)
                 break;
         }
     }
